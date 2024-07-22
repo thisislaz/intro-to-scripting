6ef2f45b7f07fbf82509e589cdff4268d29b7c0a
Lazaro Alvarez <99672593+thisislaz@users.noreply.github.com>
2024-07-21 19:42:46 -0500

    Added chapter 3 notres

---
+++ zyBooks/zyBooks-3/3.5 - booleans.py
@@ -0,0 +1,7 @@
+young = (input() == 'True')
+famous = (input() == 'True')
+
+if young == 'True' and famous == 'True':
+    print('You must be rich!')
+else:
+    print('There is always the lottery...')
